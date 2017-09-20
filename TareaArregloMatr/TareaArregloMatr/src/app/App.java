f6b689688814a2740ff7a6eeaf0f3bcf779efe02
Laura <j_laura14@outlook.com>
2017-09-19 22:18:12 -0600

    TIS41M UTCH
    ED Projects

---
+++ TareaArregloMatr/TareaArregloMatr/src/app/App.java
@@ -0,0 +1,20 @@
+package app;
+
+
+
+public class App {
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 
+		Matriz m = new Matriz();
+	
+		m.Diagonalsec();
+		m.MatrizNormal();
+		m.SumaNumPar();
+		m.MayorMenor();
+		
+			
+	}
+}
